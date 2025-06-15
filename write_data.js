e3ee417e65828714999791228492e9fcd8ba9c6c
conert mavuso <conertmavuso@gmail.com>
2025-06-15 16:03:21 +0200

    displaying on the html

--- write_data.js
+++ write_data.js
@@ -2,27 +2,65 @@ import {retrieveData, sendData} from "./get_data.js"
 
 async function displayData(){
 
+  let youngest 
+  let oldest
+  let pizza
+  let pasta
+  let wors
+  let movies 
+  let radio
+  let eatOut
+  let tv
 
-    document.getElementById("age").innerHTML = "lllllllllllllllll"
-    console.log(document.getElementById("age").innerHTML)
     let jsonFile = await retrieveData()
-    console.log(jsonFile)
+        const size = jsonFile.length;
+    console.log(jsonFile.size)
 
     document.getElementById("age").innerHTML = "lllllllllllllllll"
     document.getElementById("oldest").innerHTML = "lllllllllllllllll"
-    document.getElementById("youngest").innerHTML = `it is ${jsonFile.radio}`
+    document.getElementById("youngest").innerHTML = `it is ${jsonFile}`
+  
+
+    jsonFile.forEach((jsonFile) => {
+    console.log(jsonFile.email)
+    youngest = jsonFile.dateOfBirth
+  });
+      document.getElementById("youngest").innerHTML = `it ifdss ${youngest}`
+      document.getElementById("survey_no").innerHTML = `Total number of surveys  ${size}`
+      document.getElementById("lll").innerHTML = ` ${size}`
+
+
 
     return await retrieveData()
+
   }
 
 
-//   displayData()
-  async function write() {
-    let ll= await displayData()
-    console.log(ll.radio)
-     document.getElementById("age").innerHTML = "lllllllllllllllll"
-    document.getElementById("oldest").innerHTML = "lllllllllllllllll"
-    document.getElementById("youngest").innerHTML = `it is ${ll.radio}`
+  displayData()
+  // async function write() {
+  //   let ll= await displayData()
+  //   console.log(ll.radio)
+  //    document.getElementById("age").innerHTML = "lllllllllllllllll"
+  //   document.getElementById("oldest").innerHTML = "lllllllllllllllll"
+  //   document.getElementById("youngest").innerHTML = `it is ${ll}`
     
-  }
-  write()
+  // }
+  // write()
+
+
+
+  async function displayEmails() {
+  const users = await retrieveData();
+  // const emailListDiv = document.getElementById("email-list");
+
+  // Clear any previous content
+  // emailListDiv.innerHTML = "";
+
+  users.forEach((user) => {
+    // const p = document.createElement("p");
+    // p.textContent = user.email;
+    // emailListDiv.appendChild(p);
+    console.log(user.email)
+  });
+}
+displayEmails()
